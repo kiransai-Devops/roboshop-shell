51e23d3e77bb43e09d9d341ded8a21d304370b86
kiransaiveerandi <kiransaiveerandi@gmail.com>
2025-09-25 19:41:57 +0530

    mongo

--- mongo.sh
+++ mongo.sh
@@ -8,7 +8,7 @@ N="/e[0m"
 
 LOGS_FLODER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="#LOGS_FLODER/$SCRIPT_NAME.log
+LOG_FILE="$LOGS_FLODER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
@@ -22,10 +22,10 @@ fi
 VALIDATE(){ 
 
   if [ $1 -ne 0 ]; then
-      echo -e "$2 ... $R FAILURE $N" |  tee -a $LOG_FILE
+      echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
       exit 1
   else 
-      echo -e "$2... $G SUCCESS $N" | tee -a $LOG_FILE
+      echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
   fi
 }  
 
