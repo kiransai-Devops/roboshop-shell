dbf5582a671ef44118bbe18f3eb729d9d81bd2e4
kiransaiveerandi <kiransaiveerandi@gmail.com>
2025-09-26 20:59:01 +0530

    roboshop

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -10,7 +10,7 @@ N="/e[0m"
 
 LOGS_FOLDER="/var/log/roboshop-shell"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-START_TIME=$(date %+s)
+START_TIME=$(date +%s)
 SCRIPT_DIR=$PWD
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
